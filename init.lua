007a1b1eded43318dac157de1d1b969011383a9c
Antonius Naumann <git@antonius.dev>
2023-10-13 11:30:37 +0200

    Close telescope with single escape

--- init.lua
+++ init.lua
@@ -3,3 +3,14 @@ require("config.lazy")
 vim.cmd("hi Normal guibg=#14191e")
 vim.cmd("hi NormalNC guibg=#181b21")
 vim.cmd("hi NormalFloat guibg=#212429")
+
+local telescopeActions = require("telescope.actions")
+require("telescope").setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = telescopeActions.close,
+      },
+    },
+  },
+})
